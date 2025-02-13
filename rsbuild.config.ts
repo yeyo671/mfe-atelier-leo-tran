dc7e83bf3078d1e3629e716f75a6959440ed0214
yeyo671 <leotran671@gmail.com>
2025-02-13 09:15:46 +0100

    first commit

---
+++ rsbuild.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'federation_provider',
+      exposes: {
+        './button': './src/button.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+});
